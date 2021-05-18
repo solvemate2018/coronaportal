3728c103ec8e6bc472c003ea322bfdae1084c58d
franekl <73118808+franekl@users.noreply.github.com>
2021-05-18 23:11:13 +0200

    Update ITestAppointmentRepo.java

--- src/main/java/com/coronaportal/repositories/ITestAppointmentRepo.java
+++ src/main/java/com/coronaportal/repositories/ITestAppointmentRepo.java
@@ -6,4 +6,20 @@ import com.coronaportal.models.TestResult;
 import java.util.List;
 
 public interface ITestAppointmentRepo {
+
+    List<TestAppointment> fetchAppointments(String cpr);
+
+    List<TestAppointment> fetchAppointments();
+
+    void makeAppointmentForPerson(TestAppointment testAppointment);
+
+    void deleteAppointment(int id);
+
+    List<TestAppointment> fetchAppointments(int test_center_id);
+
+    List<TestAppointment> fetchDailyAppointments(int test_center_id);
+
+    List<TestAppointment> fetchDailyAppointments(int test_center_id, LocalDateTime test_time);
+
 }
+
