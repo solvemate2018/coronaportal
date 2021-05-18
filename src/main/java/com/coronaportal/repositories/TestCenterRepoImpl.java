0785af40a700b62ef2221a28fb888c0e089d22a8
franekl <73118808+franekl@users.noreply.github.com>
2021-05-18 23:49:40 +0200

    Update TestCenterRepoImpl.java

--- src/main/java/com/coronaportal/repositories/TestCenterRepoImpl.java
+++ src/main/java/com/coronaportal/repositories/TestCenterRepoImpl.java
@@ -3,6 +3,7 @@ package com.coronaportal.repositories;
 import com.coronaportal.models.TestAppointment;
 import com.coronaportal.models.TestCenter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
