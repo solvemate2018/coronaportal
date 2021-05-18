40894890cf05beab8a78c6f0d4db5777b8628d38
franekl <73118808+franekl@users.noreply.github.com>
2021-05-18 23:23:10 +0200

    Update TestResultRepoImpl.java

--- src/main/java/com/coronaportal/repositories/TestResultRepoImpl.java
+++ src/main/java/com/coronaportal/repositories/TestResultRepoImpl.java
@@ -8,12 +8,46 @@ import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
-
 import java.util.List;
 import java.util.Map;
 
 @Repository
 public class TestResultRepoImpl implements ITestResultRepo{
+
     @Autowired
     JdbcTemplate template;
+
+    @Override
+    public List<TestResult> fetchResults() {
+        String sql="SELECT * FROM coronaportal.test_result";
+        RowMapper<TestResult> rowMapper = new BeanPropertyRowMapper<>(TestResult.class);
+        return template.query(sql,rowMapper);
+    }
+
+    @Override
+    public List<TestResult> fetchResult(String cpr) {
+        String sql="SELECT * FROM coronaportal.test_result WHERE person_cpr = ?";
+        RowMapper<TestResult> rowMapper = new BeanPropertyRowMapper<>(TestResult.class);
+        return template.query(sql,rowMapper,cpr);
+    }
+
+    @Override
+    public void addResult(TestResult testResult) {
+        String sql="INSERT INTO coronaportal.test_result(id, test_appointments_id, time_of_result, result) VALUES(?,?,?,?)";
+        template.update(sql,testResult.getId(), testResult.getTest_appointment_id(), testResult.getTime_of_result(), testResult.getResult());
+    }
+
+    @Override
+    public void editResult(int id, String result, TestResult testResult) {
+        String sql="UPDATE coronaportal.test_result SET id=?, test_appointments_id=?,time_of_result=?, result=? WHERE id=?";
+        template.update(sql,testResult.getId(), testResult.getTest_appointment_id(), testResult.getTime_of_result(), testResult.getResult(), id);
+    }
+
+    @Override
+    public List<TestResult> fetchResult(int test_appointment_id) {
+        String sql="SELECT * FROM coronaportal.test_result WHERE test_appointments_id = ?";
+        RowMapper<TestResult> rowMapper = new BeanPropertyRowMapper<>(TestResult.class);
+        return template.query(sql, rowMapper, test_appointment_id);
+    }
 }
+
