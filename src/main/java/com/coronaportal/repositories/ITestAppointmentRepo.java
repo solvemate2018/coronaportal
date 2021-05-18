43c01e64f0ae348e47a394ceec8eb56c3fcec203
franekl <73118808+franekl@users.noreply.github.com>
2021-05-18 23:15:10 +0200

    Update ITestAppointmentRepo.java

--- src/main/java/com/coronaportal/repositories/ITestAppointmentRepo.java
+++ src/main/java/com/coronaportal/repositories/ITestAppointmentRepo.java
@@ -2,7 +2,7 @@ package com.coronaportal.repositories;
 
 import com.coronaportal.models.TestAppointment;
 import com.coronaportal.models.TestResult;
-
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface ITestAppointmentRepo {
